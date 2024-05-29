3d3a58a6d310087ff5428f09881eb7273c681e60
xiaogua07 <xiaogua07@outlook.com>
2024-05-29 13:41:58 +0800

    Update lu's change bed date.

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
         // 换床单
         {
           title:  '🛏️',
-          lastDate: '2024-05-03',
+          lastDate: '2024-05-26',
         },
       ]
     },
